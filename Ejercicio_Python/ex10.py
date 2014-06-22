6120faa423db2515e4f16f0d6f4a9f18d4d24ec3
Mabo <miguelab.17@gmail.com>
2014-06-22 10:18:59 -0500

    Ejercicio 10

---
+++ Ejercicio_Python/ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\tI'm tabbed in."#\t cambio de sangria
+persian_cat = "I'm split\non a line."#Cambia de renglon despues de \
+backslash_cat = "I'm \\ a \\ cat."#Pone un espacio e imprime un \
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
